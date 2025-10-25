d8f6627fa9f6887f108f73695443722a2435d1c7
Adithya Giri <adithyag@berkeley.edu>
2025-10-25 12:09:50 -0700

    Connected depth to input posture

--- posture_detector/api/get_depth.py
+++ posture_detector/api/get_depth.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from PIL import Image
 import requests
 import base64
@@ -7,6 +7,7 @@ import torch
 from torch import nn
 import numpy as np
 from transformers import pipeline, SegformerImageProcessor, SegformerForSemanticSegmentation
+import os
 
 print(torch.__version__)
 print(torch.torch.backends.mps.is_available())
@@ -23,8 +24,7 @@ def get_depth():
     else:
         device = "cpu"
     pipe = pipeline(task="depth-estimation", model="depth-anything/Depth-Anything-V2-Metric-Indoor-Base-hf", device = device)
-    url = 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6'
-    image = Image.open(requests.get(url, stream=True).raw)
+    image = Image.open(get_most_recent_file(f'{get_most_recent_dir("../storage/sessions/")}/frames'))
     depth = pipe(image)["depth"]
     depth.show()
     return "completed"
@@ -32,19 +32,20 @@ def get_depth():
 @app.route('/api/get_face', methods=['GET'])
 def get_feature():
     print("started")
+    feature = request.args.get('feature')
     if (torch.cuda.is_available()):
         device = torch.device("cuda")
     elif (torch.backends.mps.is_available()):
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
+    
     image_processor = SegformerImageProcessor.from_pretrained("jonathandinu/face-parsing")
     model = SegformerForSemanticSegmentation.from_pretrained("jonathandinu/face-parsing")
     model.to(device)
 
     # expects a PIL.Image or torch.Tensor
-    url = "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6"
-    image = Image.open(requests.get(url, stream=True).raw)
+    image = Image.open(get_most_recent_file(f'{get_most_recent_dir("../storage/sessions/")}/frames'))
 
     # run inference on image
     inputs = image_processor(images=image, return_tensors="pt").to(device)
@@ -61,10 +62,38 @@ def get_feature():
     labels = upsampled_logits.argmax(dim=1)[0]
 
     # move to CPU to visualize in matplotlib
-    labels_viz = (labels.cpu().numpy() == 18).astype(np.uint8)*255
+    print(feature)
+    labels_viz = (labels.cpu().numpy() == int(feature)).astype(np.uint8)*255
     print(labels_viz)
     img = Image.fromarray(labels_viz, 'L')
     img.show()
     return "finished"
+
+def get_most_recent_file(directory_path):
+
+    if not os.path.isdir(directory_path):
+        print(f"Error: Directory '{directory_path}' not found.")
+        return None
+    files = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+    if not files:
+        print(f"No files found in directory '{directory_path}'.")
+        return None
+
+    most_recent_file = max(files, key=os.path.getmtime)
+    return most_recent_file
+
+def get_most_recent_dir(directory_path):
+
+    if not os.path.isdir(directory_path):
+        print(f"Error: Directory '{directory_path}' not found.")
+        return None
+    dirs = [os.path.join(directory_path, f) for f in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, f))]
+    if not dirs:
+        print(f"No files found in directory '{directory_path}'.")
+        return None
+
+    most_recent_dir = max(dirs, key=os.path.getmtime)
+    return most_recent_dir
+
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
